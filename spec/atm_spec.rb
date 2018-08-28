05fc9b5a539a7524abf5897555a0271b056894e6
Shirmen <shirmenchen@gmail.com>
2018-08-28 09:55:03 +0200

    passing initial withdraw test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -4,4 +4,8 @@ describe Atm do
     it 'has $1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
+    it 'funds should be reduced at withdraw' do
+        subject.withdraw 50
+        expect(subject.funds).to eq 950
+    end
 end
