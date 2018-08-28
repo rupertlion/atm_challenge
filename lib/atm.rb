c292346362db1c12f557c240ed8b0822c6a09eb0
Shirmen <shirmenchen@gmail.com>
2018-08-28 11:08:54 +0200

    replect the withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,14 +5,26 @@ class Atm
     def initialize
         @funds = 1000
     end
-    def withdraw (amount, account)
+   
+    def withdraw (amount,account)
         case
-        when amount > account.balance
+        when insufficient_funds_in_account?(amount, account)
             return
         else
-            @funds -= amount
-            account.balance = account.balance - amount
-            {status: true, message: 'Success', date: Date.today, amount: amount}
+            perform_transaction(amount, account)   
         end
+    end 
+    
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
     end
+
+    def perform_transaction(amount, account)   
+        @funds -= amount
+        account.balance = account.balance - amount
+        {status: true, message: 'Success', date: Date.today, amount: amount}
+    end 
+
 end
