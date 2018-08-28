114d07ac07996d91a5f34d3de3573e7f13358f8c
rupertlion <rupertlion@gmail.com>
2018-08-28 10:43:32 +0200

    Passing available funds test

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,9 +1,18 @@
+require 'date'
+
 class Atm
     attr_accessor :funds
     def initialize
         @funds = 1000
     end
-    def withdraw (amount)
-        @funds -= amount
+    def withdraw (amount, account)
+        case
+        when amount > account.balance
+            return
+        else
+            @funds -= amount
+            account.balance = account.balance - amount
+            {status: true, message: 'Success', date: Date.today, amount: amount}
+        end
     end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,11 +1,26 @@
 require './lib/atm.rb'
 
 describe Atm do
+    
+    let(:account) { instance_double('Account') }
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
     it 'has $1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
+    
     it 'funds should be reduced at withdraw' do
-        subject.withdraw 50
+        subject.withdraw(50, account)
         expect(subject.funds).to eq 950
     end
+
+    it 'allow funds to be released only if available in account' do
+        expected_output = {status: true, message: 'Success', date: Date.today, amount: 45}
+        expect(subject.withdraw(45, account)).to eq expected_output
+    end
+
 end
