60390022883009631642bf9577c1054737dbe4d6
rupertlion <rupertlion@gmail.com>
2018-08-29 13:32:21 +0200

    Passing expiry date on initialise

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,30 @@
+require 'date'
+
 class Account
+
+    STANDARD_VALIDITY_YRS = 5
 
     attr_accessor :pin_code, :exp_date, :account_status, :name
    
     def initialize(attrs = {})
         @name = set_owner(attrs[:owner])
         @pin_code = rand(1000..9999)
-        @exp_date = '04/20'
+        @exp_date = set_expire_date
         @account_status = :active
     end
 
-     private
+    private
 
     def set_owner(obj)
         obj == nil ?  missing_owner : @owner = obj
     end
 
-      def missing_owner
+    def missing_owner
          raise "An Account owner is required"
-     end
+    end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,17 +9,18 @@ describe Account do
         expect(subject.name).to eq person
     end
 
-    # it 'account has a pin code' do
-    #     expect(subject.pin_code).to eq rand(1000..9999)
-    # end
-
     it 'account is active' do
         expect(subject.account_status).to eq :active
     end
 
-    it 'account expires in the future' do
-        expect(subject.exp_date).to eq '04/20'
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
     end
+    
+    # it 'account expires in the future' do
+    #     expect(subject.exp_date).to eq '04/20'
+    # end
 
     it 'check length of pin_code' do
         pin_code_length = Math.log10(subject.pin_code).to_i + 1
